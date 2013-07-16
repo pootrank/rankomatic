a22c9c15ac34dbdc76708c9920e7d33b36b07377
Cameron Jeffers <cwjeffers18@gmail.com>
2013-07-16 14:52:27 -0700

    updated User instance method documentation

--- rankomatic/models.py
+++ rankomatic/models.py
@@ -7,12 +7,12 @@ Defines the models for use with the Optimality Theory constraint-ranking
 website. They are implemented for MongoDB using MongoEngine. As of now the
 models are User, Tableaux, and Candidate.
 
-User: Self-explanatory. A user of the website.
-Tableaux: Represents a user's tableaux or dataset. Consists of a list of
-          constraint names and a list of Candidates.
 Candidate: A single row from the Tableaux, in OT terms an input-outuput pair.
            Also contains whether or not the output is optimal for the input
            and the violation vector for the constraint set of the Tableaux.
+Tableaux: Represents a user's tableaux or dataset. Consists of a list of
+          constraint names and a list of Candidates.
+User: Self-explanatory. A user of the website.
 """
 #TODO make sure documentation is up to date.
 
@@ -69,6 +69,10 @@ class User(DynamicDocument):
     datasets = ListField(EmbeddedDocumentField(Tableaux))
 
     def set_password(self, password):
+        """
+        Adds a randomly generated salt to the User, then appends it to the
+        password before hashing it, then saves the digest.
+        """
         self.salt = os.urandom(64)
         h = hashlib.sha512()
         password += self.salt
@@ -77,6 +81,10 @@ class User(DynamicDocument):
 
 
     def is_password_valid(self, guess):
+        """
+        Appends the user's salt to the guess before hashing it, then compares
+        it with the stored password digest.
+        """
         h = hashlib.sha512()
         guess += self.salt
         h.update(guess)
