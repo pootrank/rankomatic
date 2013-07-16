4e3cb0380df1e2db5834ae580d5e84aa566c0796
Cameron Jeffers <cwjeffers18@gmail.com>
2013-07-15 17:29:11 -0700

    updated documentation on rankomatic.py

--- rankomatic.py
+++ rankomatic.py
@@ -1,3 +1,19 @@
+"""
+File: __init__.py
+Author: Cameron Jeffers
+Email: cwjeffers18@gmail.com
+
+The main file for the Optimality Theory (OT) constraint-ranking website.
+Implemented using Flask, this website is built on a Python implementation of
+Alex Djalali's solution to the ranking problem in theoretical OT. For the
+details of his solution, see his paper "A constructive solution to the ranking
+problem in Partial Order Optimality Theory" (2013) which can be found at
+http://stanford.edu/~djalali/publications.html. For the source of his
+implementation, refer to https://github.com/alexdjalali/OT.
+
+This file initiates the app and database connection, defines the configuration,
+and imports and attaches the desired blueprints which contain the views.
+"""
 from flask import Flask, render_template, request
 from flask.ext.mongoengine import MongoEngine
 from models import tableaux_form
@@ -8,8 +24,6 @@ app.config['SECRET_KEY'] = 'keepThisSecret'
 app.config['DEBUG'] = True
 
 db = MongoEngine(app)
-
-
 
 
 
