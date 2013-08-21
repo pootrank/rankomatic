7ff9d8a2a0d87f1693ffddbb2ac83ea38d47bc9c
Cameron Jeffers <cwjeffers18@gmail.com>
2013-08-21 10:40:45 -0700

    added tempfile creation

--- rankomatic/tools.py
+++ rankomatic/tools.py
@@ -76,7 +76,8 @@ class CalculatorView(MethodView):
         cons = dict((i+1, v) for i, v in enumerate(constraints))
         for i, gram in enumerate(grammars):
             graph = self.make_graph(gram, cons)
-            #with tempfile.TemporaryFile() as tf:
+            with tempfile.TemporaryFile() as tf:
+                pass
                 #graph.draw(tf, format='png')
                 #tf.seek(0)
                 #filename = 'grammar%d.png' % i
