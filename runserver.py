fba7584f00cd120b23c1a719dd451359784bfc6f
cjeffers <cwjeffers18@gmail.com>
2015-01-19 13:53:17 -0700

    Check for running workers on import of runserver

--- runserver.py
+++ runserver.py
@@ -2,8 +2,8 @@ from rankomatic import app
 from otorderd_logd import daemon_is_running
 
 
+if not daemon_is_running(app.config['WORKER_ADDRESS']):
+    raise Exception("Run otorderd before starting server")
+
 if __name__ == '__main__':
-    if daemon_is_running(app.config['WORKER_ADDRESS']):
-        app.run(threaded=True)
-    else:
-        raise Exception("Run otorderd before starting server")
+    app.run(threaded=True)
