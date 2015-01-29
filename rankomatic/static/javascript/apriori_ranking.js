6468827c3533e32cbd62ef867cce82710addf524
cjeffers <cwjeffers18@gmail.com>
2015-01-29 12:48:24 -0700

    Escape row and col names for JQuery in a priori table

--- rankomatic/static/javascript/apriori_ranking.js
+++ rankomatic/static/javascript/apriori_ranking.js
@@ -22,6 +22,14 @@
         $("#apriori_page").show();
     });
 
+    function escape_selector(str) {
+        if (str) {
+            return str.replace(/([ !"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~`'"])/g,
+                       '\\$&');
+        }
+        return str;
+    }
+
     function show_apriori_table() {
         var constraints = $("#tableaux_header th.constraint").map(function() {
                 return $(this).find("input").val();
@@ -39,53 +47,48 @@
         $("#header_label").attr('colspan', constraints.length);
         deactivate_diagonal();
         set_apriori_listeners();
-        $("input.apriori_check").hide();
         $("#ranking_required_alert").hide();
     }
 
     function deactivate_diagonal() {
-        $("#apriori_table input.apriori_check").each(function() {
+        $("#apriori_table td.checkbox_container").each(function() {
             coords = get_coordinates($(this))
             if (coords.row === coords.col) {
-                deactivate_cell($(this).closest("td"));
+                deactivate_cell($(this));
             }
         });
     }
 
     function get_coordinates($target) {
-        if (!$target.is('input')) {
-            $target = $target.find('input');
-        }
         var col = $target.attr('name');
         var row = $target.closest("tr").attr('id').slice(4);
         return {'row': row, 'col': col}
     }
 
     function get_cell(row, col) {
+        console.log(row, col);
+        row = escape_selector(row);
+        col = escape_selector(col);
         var row_selector = "tr#" + "row_" + row;
-        var input_selector = "input[name=" + col + "]";
+        var cell_selector = "td[name=" + col + "]";
+        console.log(row_selector, cell_selector);
         var $row = $(row_selector);
-        var $input = $row.find(input_selector);
-        return $input.closest("td");
+        var $cell = $row.find(cell_selector);
+        return $cell;
     }
 
     function deactivate_cell($to_deactivate) {
         $to_deactivate.removeClass("checked unchecked");
         $to_deactivate.addClass("deactivated");
-        $to_deactivate.find("input").attr("disabled", "true");
     }
 
     function reactivate_cell($cell) {
-        $cell.find("input").attr("diabled", "false");
         $cell.removeClass("deactivated")
     }
 
     function set_apriori_listeners() {
         $("#apriori_table").click(function(event) {
             var $target = $(event.target);
-            if ($target.is('input')) {
-                $target = $target.closest("td");
-            }
             if ($target.is('td.checkbox_container')) {
                 if (!$target.hasClass("deactivated")) {
                     $("#ranking_required_alert").hide();
@@ -132,15 +135,12 @@
         reactivate_cell($cell);
         $cell.removeClass("checked");
         $cell.addClass("unchecked");
-        $input = $cell.find("input");
-        $input.prop('checked', false);
-        $cell.html($input[0].outerHTML);
+        $cell.html("");
     }
 
     function check_cell($cell) {
         $cell.removeClass("unchecked");
         $cell.addClass("checked");
-        $cell.find("input").prop('checked', true);
-        $cell.prepend("&#10003;")
+        $cell.html("&#10003;")
     }
 })(this, this.document)
